60fe460eac044843337fbd5b819dac31d0e9ca99
2emeagauche <pychassaigne@gmail.com>
2025-04-11 15:33:49 +0200

    setting for deploy on Railway.app

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-	dialect: "sqlite",
-	storage: './database.sqlite'
+	development:{
+		dialect: "sqlite",
+		storage: './database.sqlite'
+	},
+	production:{
+		dialect: "sqlite",
+		storage: process.env.RAILWAY_VOLUME_MOUNT_PATH ? 
+		`${process.env.RAILWAY_VOLUME_MOUNT_PATH}/database.sqlite` : 
+		"./database.sqlite"
+	}
 };
